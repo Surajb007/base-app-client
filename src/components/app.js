54a23955b3f2baf9171e265b88476a6f7d32cb41
Suraj <suraj9629@gmail.com>
2019-07-29 02:00:24 +0530

    Change logic to show whether loggedin or not

--- src/components/app.js
+++ src/components/app.js
@@ -2,15 +2,37 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
+import axios from "axios";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedInStatus: "Not Logged In",
+      loggedInStatus: "NOT_LOGGED_IN",
       user: {}
     };
   }
+  checkLoginStatus = () => {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then(res => {
+        if (
+          res.data.logged_in &&
+          this.state.loggedInStatus === "NOT_LOGGED_IN"
+        ) {
+          this.setState({ loggedInStatus: "LOGGED_IN", user: res.data.user });
+        } else if (
+          !res.data.logged_in &&
+          this.state.loggedInStatus === "LOGGED_IN"
+        ) {
+          this.setState({ loggedInStatus: "NOT_LOGGED_IN", user: {} });
+        }
+      })
+      .catch(err => console.log(err));
+  };
+  componentDidMount = () => {
+    this.checkLoginStatus();
+  };
   handleLogin = data => {
     this.setState({ loggedInStatus: "Logged_In", user: data.user });
   };
